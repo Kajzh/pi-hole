52d099bfa17525c7aed5c5733817b852f598d776
Kajzh <107438285+Kajzh@users.noreply.github.com>
2022-08-14 23:00:24 +0000

    Update uninstall.sh
    Signed-off-by: Kajzh <107438285+Kajzh@users.noreply.github.com>

--- automated install/uninstall.sh
+++ automated install/uninstall.sh
@@ -3,7 +3,7 @@
 # (c) 2017 Pi-hole, LLC (https://pi-hole.net)
 # Network-wide ad blocking via your own hardware.
 #
-# Completely uninstalls Pi-hole
+# Completely uninstlls Pi-hole
 #
 # This file is copyright under the latest version of the EUPL.
 # Please see LICENSE file for your rights under this license.
